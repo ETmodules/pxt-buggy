99cf040861a465c34b1246e3f16cc82fc0ddb01e
ETmodules <etmodules@outlook.com>
2025-01-01 12:22:17 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -5,6 +5,27 @@ namespace EtEdrive {
 
     let MODULE: string = "EtBuggy"
 
+    enum Direction {
+        //% block="forward"
+        //% block.loc.nl="vooruit"
+        Forward,
+        //% block="reverse"
+        //% block.loc.nl="achteruit"
+        Reverse
+    }
+
+    enum Turning {
+        //% block="straight"
+        //% block.loc.nl="rechtdoor"
+        Straight,
+        //% block="left"
+        //% block.loc.nl="links"
+        Left,
+        //% block="right"
+        //% block.loc.nl="rechts"
+        Right
+    }
+
     //% block="ID"
     //% block.loc.nl="ID"
     export function id(): string {
@@ -18,12 +39,35 @@ namespace EtEdrive {
         MODULE = id
     }
 
+    //% block="stop %id"
+    //% block.loc.nl="stop %id"
+    //% id.defl="EtBuggy"
+    export function stop(id: string) {
+        EtCommon.setValue(id, "stop", "")
+    }
+
     //% block="set speed of %id to %speed \\%"
-    //% block.loc.nl="stel de snelheid van %id in op %speed \\%"
+    //% block.loc.nl="rijd %id met snelheid %speed \\%"
     //% id.defl="EtBuggy"
     //% speed.min=0 speed.max=100 speed.defl=50
-    export function setSpeed(id: string, speed: number) {
-        EtCommon.setValue(id, "speed", "20")
+    export function setSpeedPerc(id: string, speed: number) {
+        EtCommon.setValue(id, "perc", speed.toString())
+    }
+
+    //% block="set speed of %id to %speed m/s"
+    //% block.loc.nl="rijd %id met snelheid %speed m/s"
+    //% id.defl="EtBuggy"
+    //% speed.min=0 speed.max=2 speed.defl=1
+    export function setSpeedMps(id: string, speed: number) {
+        EtCommon.setValue(id, "mps", speed.toString())
+    }
+
+    //% block="set speed of %id to %speed km/hr"
+    //% block.loc.nl="rijd %id met snelheid %speed km/uur"
+    //% id.defl="EtBuggy"
+    //% speed.min=0 speed.max=7 speed.defl=4
+    export function setSpeedKph(id: string, speed: number) {
+        EtCommon.setValue(id, "kph", speed.toString())
     }
 
     //% block="speed of %id"
@@ -37,4 +81,30 @@ namespace EtEdrive {
         return parseFloat(speed)
     }
 
+    //% block="move %id %dir"
+    //% block.loc.nl="rijd met %id %dir"
+    //% id.defl="EtBuggy"
+    export function setDirection(id: string, dir: Direction) {
+        if (dir == Direction.Forward)
+            EtCommon.setValue(id, "forward", "")
+        else
+            EtCommon.setValue(id, "reverse", "")
+    }
+
+    //% block="turn %id %degr to the %turn"
+    //% block.loc.nl="draai %id %degr graden naar %turn"
+    //% id.defl="EtBuggy"
+    export function setTurning(id: string, degr: number, turn: Turning) {
+        if (turn == Turning.Left)
+            EtCommon.setValue(id, "left", degr.toString())
+        else
+            EtCommon.setValue(id, "right", degr.toString())
+    }
+
+    //% block="move %id straight"
+    //% block.loc.nl="rijd met %id rechtdoor"
+    //% id.defl="EtBuggy"
+    export function setStraight(id: string) {
+        EtCommon.setValue(id, "straight", "")
+    }
 }
